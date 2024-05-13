117961074aa621b4e49172ad1bab079074cd28e2
blackbeardsyed1 <syed123khizar@gmail.com>
2024-05-13 19:28:33 +0500

    Data Validation Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.mlProject import logger
 from src.mlProject.pipeline._01_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.mlProject.pipeline._02_data_validation_pipeline import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -12,3 +13,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
